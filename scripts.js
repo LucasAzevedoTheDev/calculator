c23d76f334aefb42e99bd6c66f1573246073192c
Lucas Azevedo <lucasazevedominas@gmail.com>
2026-04-20 16:32:07 -0300

    add all the button variables to the scripts sheet

--- scripts.js
+++ scripts.js
@@ -38,8 +38,27 @@ function operate(oper, a, b) {
   }
 };
 
-let display = document.querySelector(".display")
+let display = document.querySelector(".display");
 
-display.textContent = "12345"
+display.textContent = "12345";
 
 // BUTTONS
+let delButton = document.querySelector(".del");
+let acButton = document.querySelector(".ac");
+let percentButton = document.querySelector(".%");
+let divisorButton = document.querySelector(".divisor");
+let button7 = document.querySelector(".7");
+let button8 = document.querySelector(".8");
+let button9 = document.querySelector(".9");
+let multiplyButton = document.querySelector(".x");
+let button4 = document.querySelector(".4");
+let button5 = document.querySelector(".5");
+let button6 = document.querySelector(".6");
+let minusButton = document.querySelector(".-");
+let button1 = document.querySelector(".1");
+let button2 = document.querySelector(".2");
+let button3 = document.querySelector(".3");
+let plusButton = document.querySelector(".+");
+let button0 = document.querySelector(".0");
+let commaButton = document.querySelector(".,");
+let equalButton = document.querySelector(".equal");
